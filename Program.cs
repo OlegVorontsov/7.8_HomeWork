72cc5425e2fcb7ae01145d2780a7040ca660565d
Олег Воронцов <oleg-voroncov@mail.ru>
2022-12-29 10:38:25 +0300

    4 method done

--- Program.cs
+++ Program.cs
@@ -9,64 +9,15 @@ namespace _7._8_HomeWork
 {
     class Program
     {
-        static void putInfo(string fileName)
-        {
-            using (StreamWriter put = new StreamWriter(fileName, true, Encoding.UTF8))
-            {
-                char key = 'д';
-                do
-                {
-                    string line = string.Empty;
-
-                    Console.Write("\nВведите ID сотрудника: ");
-                    int ID = int.Parse(Console.ReadLine());
-
-                    string nowDate = DateTime.Now.ToShortDateString();
-                    string nowTime = DateTime.Now.ToShortTimeString();
-                    Console.WriteLine($"Дата и время добавления записи: {nowDate} {nowTime}");
-
-                    Console.Write("\nВведите Ф.И.О. сотрудника: ");
-                    string fullName = Console.ReadLine();
-
-                    Console.Write("\nВведите день рождения сотрудника: ");
-                    int dayOfBirth = int.Parse(Console.ReadLine());
-                    Console.Write("\nВведите месяц рождения сотрудника: ");
-                    int monthOfBirth = int.Parse(Console.ReadLine());
-                    Console.Write("\nВведите год рождения сотрудника: ");
-                    int yearOfBirth = int.Parse(Console.ReadLine());
-
-                    DateTime dateOfBirth = new DateTime(yearOfBirth, monthOfBirth, dayOfBirth);
-                    string dateBirth = dateOfBirth.ToShortDateString();
-
-                    int yearBirth = Convert.ToInt32(dateBirth.Substring(6));
-                    int nowYear = Convert.ToInt32(nowDate.Substring(6));
-                    int age = nowYear - yearBirth;
-                    string ageTaken = Convert.ToString(age);
-                    Console.WriteLine($"Возраст сотрудника: {ageTaken}");
-
-                    Console.Write("\nВведите рост сотрудника: ");
-                    int height = int.Parse(Console.ReadLine());
-
-                    Console.Write("\nВведите место рождения сотрудника: ");
-                    string placeBirth = Console.ReadLine();
-
-                    line = $"{ID}#" + $"{nowDate} {nowTime}#" + $"{fullName}#" + $"{ageTaken}#" + $"{height}#" + $"{dateBirth}#" + $"{placeBirth}";
-
-                    put.WriteLine(line);
-
-                    Console.Write("Продожить (н/д): ");
-                    key = Console.ReadKey(true).KeyChar;
-                } while (char.ToLower(key) == 'д');
-            }
-        }
         static void Main(string[] args)
         {
             string Path = "Справочник.txt";
             Repository rep = new Repository(Path);
 
             Console.Write("Введите '0', чтобы создать файл Справочник сотрудников\n" +
-                          "Введите '1', чтобы вывести данные о сотрудниках на экран\n" +
-                          "Введите '2', чтобы внести данные о новых сотрудниках в Справочник: ");
+                          "Введите '1', чтобы вывести данные о сотрудниках из Справочника\n" +
+                          "Введите '2', чтобы внести данные о новых сотрудниках в конец Справочника\n" +
+                          "Введите '3', чтобы вывести данные о сотруднике по его ID\n");
             var input = Console.ReadLine();
 
             switch (input)
@@ -78,13 +29,20 @@ namespace _7._8_HomeWork
                     rep.Load();
                     rep.getInfo();
                     break;
+                case "2":
+                    rep.CreateRepository(Path);
+                    rep.putWorker(Path);
+                    break;
+                case "3":
+                    rep.CreateRepository(Path);
+                    Console.Write("Введите ID сотрудника: ");
+                    int ID = int.Parse(Console.ReadLine());
+                    rep.getWorkerByID(Path, ID);
+                    break;
                 default:
-                    Console.Write("Вы ввели не '1' и не '2'");
+                    Console.Write("Вы ввели недопустимый символ");
                     break;
             }
-
-
-
             Console.ReadLine();
         }
     }
--- Repository.cs
+++ Repository.cs
@@ -49,7 +49,7 @@ namespace _7._8_HomeWork
             }
             else
             {
-                Console.WriteLine("Файл уже существует");
+                Console.WriteLine("Файл Справочник существует");
             }
         }
 
@@ -74,7 +74,6 @@ namespace _7._8_HomeWork
         /// <summary>
         /// Вывод Справочника сотрудников в консоль
         /// </summary>
-        /// <param name="Path"></param>
         public void getInfo()
         {
             Console.WriteLine($"{this.titles[0],5}{this.titles[1],20}{this.titles[2],25}{this.titles[3],20}{this.titles[4],8}{this.titles[5],17}{this.titles[6],20}");
@@ -86,7 +85,7 @@ namespace _7._8_HomeWork
         }
 
         /// <summary>
-        /// Увеличение количества сотрудников
+        /// Увеличение массива сотрудников
         /// </summary>
         /// <param name="Flag"></param>
         private void Resize(bool Flag)
@@ -98,7 +97,7 @@ namespace _7._8_HomeWork
         }
 
         /// <summary>
-        /// Добавление сотрудника
+        /// Добавление сотрудника в массив
         /// </summary>
         /// <param name="NewWorker"></param>
         public void Add(Worker NewWorker)
@@ -106,6 +105,93 @@ namespace _7._8_HomeWork
             this.Resize(index >= this.Workers.Length);
             this.Workers[index] = NewWorker;
             this.index++;
+        }
+
+        /// <summary>
+        /// Добавление данных о сотрудниках в конец Справочника
+        /// </summary>
+        /// <param name="Path"></param>
+        public void putWorker(string Path)
+        {
+            using (StreamWriter put = new StreamWriter(Path, true, Encoding.UTF8))
+            {
+                char key = 'д';
+                do
+                {
+                    string line = string.Empty;
+
+                    Console.Write("\nВведите ID сотрудника: ");
+                    int ID = int.Parse(Console.ReadLine());
+
+                    string nowDate = DateTime.Now.ToShortDateString();
+                    string nowTime = DateTime.Now.ToShortTimeString();
+                    Console.WriteLine($"Дата и время добавления записи: {nowDate} {nowTime}");
+
+                    Console.Write("\nВведите Ф.И.О. сотрудника: ");
+                    string fullName = Console.ReadLine();
+
+                    Console.Write("\nВведите дату рождения сотрудника (дд.мм.гггг): ");
+                    DateTime enteredDate = DateTime.Parse(Console.ReadLine());
+                    string dateBirth = enteredDate.ToShortDateString();
+
+                    int yearBirth = Convert.ToInt32(dateBirth.Substring(6));
+                    int nowYear = Convert.ToInt32(nowDate.Substring(6));
+                    int age = nowYear - yearBirth;
+                    string ageTaken = Convert.ToString(age);
+                    Console.WriteLine($"Возраст сотрудника: {ageTaken}");
+
+                    Console.Write("\nВведите рост сотрудника: ");
+                    int height = int.Parse(Console.ReadLine());
+
+                    Console.Write("\nВведите место рождения сотрудника: ");
+                    string placeBirth = Console.ReadLine();
+
+                    line = $"{ID}#" + $"{nowDate} {nowTime}#" + $"{fullName}#" + $"{ageTaken}#" + $"{height}#" + $"{dateBirth}#" + $"{placeBirth}";
+
+                    put.WriteLine(line);
+
+                    Console.Write("Продожить (н/д): ");
+                    key = Console.ReadKey(true).KeyChar;
+                } while (char.ToLower(key) == 'д');
+            }
+        }
+
+        /// <summary>
+        /// Вывод данных о сотруднике по его ID
+        /// </summary>
+        /// <param name="Path"></param>
+        /// <param name="ID"></param>
+        public void getWorkerByID(string Path, int ID)
+        {
+            int i = 0;
+            bool IDnotFound = true;
+            bool TitlesNotPrinted = true;
+            using (StreamReader sr = new StreamReader(this.path))
+            {
+                titles = sr.ReadLine().Split(',');
+                while (!sr.EndOfStream)
+                {
+                    string[] args = sr.ReadLine().Split('#');
+
+                    Add(new Worker(args[0], args[1], args[2], args[3], args[4], args[5], args[6]));
+
+                    if (args[0] == ID.ToString())
+                    {
+                        if (TitlesNotPrinted)
+                        {
+                            Console.WriteLine($"{this.titles[0],5}{this.titles[1],20}{this.titles[2],25}{this.titles[3],20}{this.titles[4],8}{this.titles[5],17}{this.titles[6],20}");
+                            TitlesNotPrinted = false;
+                        }
+                        Console.WriteLine(this.Workers[i].Print());
+                        IDnotFound = false;
+                    }
+                    i++;
+                }
+                if (IDnotFound)
+                {
+                    Console.WriteLine($"Сотрудника с ID: {ID} нет в Справочнике");
+                }
+            }
         }
 
         #endregion
